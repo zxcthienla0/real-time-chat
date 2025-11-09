4ae3653fa4c2dc2afc6fbe5cb7e92b1da5a93234
zxcThienla0 <semasema6865@gmail.com>
2025-11-09 04:48:19 +0300

    fix Docker

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || 'database',
         port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
         logging: false
